1937c8e75bf02c4369cf2bad48aa4de0fc92bc53
roflanix <raptor-04@mail.ru>
2022-10-18 20:43:41 +0300

    Add project files.

---
+++ Beloglazoff/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Beloglazoff.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Beloglazoff/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Beloglazoff.Startup))]
+namespace Beloglazoff
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
