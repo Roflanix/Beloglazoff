9d86e8c69151785a2198bb1799dc1995042516bd
roflanix <raptor-04@mail.ru>
2022-11-22 17:09:15 +0300

    Лаба 4

--- Beloglazoff/ProductList.aspx.cs
+++ Beloglazoff/ProductList.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Beloglazoff.Models;
+using System.Web.ModelBinding;
 
 namespace Beloglazoff
 {
@@ -12,6 +14,17 @@ namespace Beloglazoff
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new Beloglazoff.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
         }
     }
 }
